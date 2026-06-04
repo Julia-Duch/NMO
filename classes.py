6a3b79661ccd46cd4bfb9e53270a3aa7128b19a1
Julia-Duch <julia.duch155@gmail.com>
2026-06-04 10:12:24 +0200

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,392 @@
+dane = {
+    "Analiza czasu trwania": [
+        {
+            "id": 1,
+            "lecturer": "Aneta Ptak-Chmielewska",
+            "day": "wtorek",
+            "start": "17:10",
+            "end": "18:40",
+            "building": "B",
+            "lecture hall": "12"
+        },
+        {
+            "id": 2,
+            "lecturer": "Aneta Ptak-Chmielewska",
+            "day": "wtorek",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "B",
+            "lecture hall": "12"
+        },
+        {
+            "id": 3,
+            "lecturer": "Aleksandra Iwanicka",
+            "day": "sroda",
+            "start": "13:30",
+            "end": "15:00",
+            "building": "C",
+            "lecture hall": "2D"
+        },
+        {
+            "id": 4,
+            "lecturer": "Aleksandra Iwanicka",
+            "day": "sroda",
+            "start": "15:20",
+            "end": "16:50",
+            "building": "C",
+            "lecture hall": "2D"
+        },
+        {
+            "id": 5,
+            "lecturer": "Wioletta Grzenda",
+            "day": "czwartek",
+            "start": "11:40",
+            "end": "13:10",
+            "building": "C",
+            "lecture hall": "2D"
+        },
+        {
+            "id": 6,
+            "lecturer": "Wioletta Grzenda",
+            "day": "czwartek",
+            "start": "13:30",
+            "end": "15:00",
+            "building": "C",
+            "lecture hall": "2D"
+        }
+    ],
+    "Analiza danych w czasie rzeczywistym": [
+        {
+            "id": 1,
+            "lecturer": "Sebastian Zajac",
+            "day": "poniedzialek",
+            "start": "08:00",
+            "end": "09:30",
+            "building": "G",
+            "lecture hall": "108"
+        },
+        {
+            "id": 2,
+            "lecturer": "Sebastian Zajac",
+            "day": "poniedzialek",
+            "start": "09:50",
+            "end": "11:20",
+            "building": "G",
+            "lecture hall": "108"
+        },
+        {
+            "id": 3,
+            "lecturer": "Sebastian Zajac",
+            "day": "poniedzialek",
+            "start": "11:40",
+            "end": "13:10",
+            "building": "G",
+            "lecture hall": "108"
+        },
+        {
+            "id": 4,
+            "lecturer": "Sebastian Zajac",
+            "day": "poniedzialek",
+            "start": "13:30",
+            "end": "15:00",
+            "building": "G",
+            "lecture hall": "108"
+        },
+        {
+            "id": 5,
+            "lecturer": "Sebastian Zajac",
+            "day": "wtorek",
+            "start": "11:40",
+            "end": "13:10",
+            "building": "B",
+            "lecture hall": "12"
+        },
+        {
+            "id": 6,
+            "lecturer": "Sebastian Zajac",
+            "day": "wtorek",
+            "start": "13:30",
+            "end": "15:00",
+            "building": "B",
+            "lecture hall": "12"
+        }
+    ],
+    "Język PL/SQL": [
+        {
+            "id": 1,
+            "lecturer": "Witold Swierzy",
+            "day": "poniedzialek",
+            "start": "17:10",
+            "end": "18:40",
+            "building": "G",
+            "lecture hall": "116"
+        },
+        {
+            "id": 2,
+            "lecturer": "Witold Swierzy",
+            "day": "poniedzialek",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "G",
+            "lecture hall": "116"
+        },
+        {
+            "id": 3,
+            "lecturer": "Witold Swierzy",
+            "day": "wtorek",
+            "start": "11:40",
+            "end": "13:10",
+            "building": "G",
+            "lecture hall": "116"
+        },
+        {
+            "id": 4,
+            "lecturer": "Witold Swierzy",
+            "day": "piatek",
+            "start": "15:20",
+            "end": "16:50",
+            "building": "G",
+            "lecture hall": "116"
+        }
+    ],
+    "Credit Scoring": [
+        {
+            "id": 1,
+            "lecturer": "Olga Momot",
+            "day": "wtorek",
+            "start": "17:10",
+            "end": "18:40",
+            "building": "G",
+            "lecture hall": "235"
+        },
+        {
+            "id": 2,
+            "lecturer": "Olga Momot",
+            "day": "wtorek",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "G",
+            "lecture hall": "235"
+        },
+        {
+            "id": 3,
+            "lecturer": "Olga Momot",
+            "day": "sroda",
+            "start": "17:10",
+            "end": "18:40",
+            "building": "C",
+            "lecture hall": "2D"
+        },
+        {
+            "id": 4,
+            "lecturer": "Olga Momot",
+            "day": "sroda",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "C",
+            "lecture hall": "4D"
+        },
+        {
+            "id": 5,
+            "lecturer": "Barbara Walczyk",
+            "day": "sroda",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "C",
+            "lecture hall": "2D"
+        },
+        {
+            "id": 6,
+            "lecturer": "Karol Przanowski",
+            "day": "piatek",
+            "start": "17:10",
+            "end": "19:40",
+            "building": "C",
+            "lecture hall": "4D"
+        }
+    ],
+    "Data Mining": [
+        {
+            "id": 1,
+            "lecturer": "Wioletta Grzenda",
+            "day": "czwartek",
+            "start": "15:20",
+            "end": "16:50",
+            "building": "C",
+            "lecture hall": "2D"
+        },
+        {
+            "id": 2,
+            "lecturer": "Wioletta Grzenda",
+            "day": "czwartek",
+            "start": "17:10",
+            "end": "19:40",
+            "building": "C",
+            "lecture hall": "2D"
+
+        },
+        {
+            "id": 3,
+            "lecturer": "Aneta Ptak-Chmielewska",
+            "day": "czwartek",
+            "start": "17:10",
+            "end": "19:40",
+            "building": "C",
+            "lecture hall": "5D"
+        },
+        {
+            "id": 4,
+            "lecturer": "Aneta Ptak-Chmielewska",
+            "day": "czwartek",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "C",
+            "lecture hall": "5D"
+        },
+        {
+            "id": 5,
+            "lecturer": "Ryszard Szupiluk",
+            "day": "piatek",
+            "start": "13:30",
+            "end": "15:00",
+            "building": "G",
+            "lecture hall": "235"
+        },
+        {
+            "id": 6,
+            "lecturer": "Ryszard Szupiluk",
+            "day": "piatek",
+            "start": "15:20",
+            "end": "16:50",
+            "building": "G",
+            "lecture hall": "235"
+        }
+    ], 
+    "Generatywna sztuczna inteligencja":[
+        {
+            "id": 1,
+            "lecturer": "Piotr Mierzejewski",
+            "day": "sroda",
+            "start": "15:20",
+            "end": "16:50",
+            "building": "C",
+            "lecture hall": "5D"
+        },
+        {
+            "id": 2,
+            "lecturer": "Piotr Mierzejewski",
+            "day": "sroda",
+            "start": "17:10",
+            "end": "18:40",
+            "building": "C",
+            "lecture hall": "5D"
+        },
+        {
+            "id": 3,
+            "lecturer": "Piotr Mierzejewski",
+            "day": "sroda",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "C",
+            "lecture hall": "5D"
+        }
+    ],
+    "Język angielski 1": [
+        {
+            "id": 1,
+            "lecturer": "Magdalena Wierzbicka",
+            "day": "poniedzialek",
+            "start": "08:00",
+            "end": "09:30",
+            "building": "S",
+            "lecture hall": "7"
+        },
+        {
+            "id": 2,
+            "lecturer": "Magdalena Wierzbicka",
+            "day": "poniedzialek",
+            "start": "13:30",
+            "end": "15:00",
+            "building": "S",
+            "lecture hall": "7"
+        }        
+    ],
+    "jezyk angielski 2": [
+        {
+            "id": 1,
+            "lecturer": "Magdalena Wierzbicka",
+            "day": "czwartek",
+            "start": "13:30",
+            "end": "15:00",
+            "building": "S",
+            "lecture hall": "21"
+        },
+        {
+            "id": 2,
+            "lecturer": "Magdalena Wierzbicka",
+            "day": "czwartek",
+            "start": "08:00",
+            "end": "09:30",
+            "building": "S",
+            "lecture hall": "21"
+        }
+    ],
+    "Nieklasyczne metody optymalizacji - wykład": [
+        {
+            "id": 1,
+            "lecturer": "Grzegorz Koloch",
+            "day": "wtorek",
+            "start": "08:55",
+            "end": "11:20",
+            "building": "G",
+            "lecture hall": "Aula II"
+        }
+    ],
+    "Nieklasyczne metody optymalizacji - ćwiczenia": [
+        {
+            "id": 1,
+            "lecturer": "Daniel Kaszyński",
+            "day": "poniedzialek",
+            "start": "15:20",
+            "end": "16:50",
+            "building": "G",
+            "lecture hall": "210"
+        },
+        {
+            "id": 2,
+            "lecturer": "Daniel Kaszyński",
+            "day": "poniedzialek",
+            "start": "17:10",
+            "end": "18:40",
+            "building": "G",
+            "lecture hall": "210"
+        },
+        {
+            "id": 3,
+            "lecturer": "Daniel Kaszyński",
+            "day": "poniedzialek",
+            "start": "19:00",
+            "end": "20:30",
+            "building": "G",
+            "lecture hall": "210"
+        },
+        {
+            "id": 4,
+            "lecturer": "Szymon Chudziak",
+            "day": "wtorek",
+            "start": "15:20",
+            "end": "16:50",
+            "building": "C",
+            "lecture hall": "2A"
+        },
+        {
+            "id": 5,
+            "lecturer": "Szymon Chudziak",
+            "day": "wtorek",
+            "start": "17:10",
+            "end": "18:40",
+            "building": "C",
+            "lecture hall": "2A"
+        }
+    ]
+}
